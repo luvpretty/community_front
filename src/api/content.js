7ada5c9cf3f793aa7560be8394d82089c5882bd9
luvpretty <975551732@qq.com>
2020-01-04 10:39:57 +0800

    上传头像前端页面开发

--- src/api/content.js
+++ src/api/content.js
@@ -5,33 +5,23 @@ import qs from 'qs'
  * 读取文章列表
  * @param {Object} options 读取文章列表接口参数
  */
-const getList = (options) => {
-  return axios.get('/public/list?' + qs.stringify(options))
-}
+const getList = (options) => axios.get('/public/list?' + qs.stringify(options))
 
-/**
- * 温馨提醒
- */
-const getTips = () => {
-  return axios.get('/public/tips')
-}
+// 温馨提醒
+const getTips = () => axios.get('/public/tips')
 
-/**
- * 本周热议
- */
-const getTop = () => {
-  return axios.get('/public/topWeek')
-}
+// 本周热议
+const getTop = () => axios.get('/public/topWeek')
 
-/**
- * 友情链接
- */
-const getLinks = () => {
-  return axios.get('/public/links')
-}
+// 友情链接
+const getLinks = () => axios.get('/public/links')
+
+// 图片上传
+const uploadImg = (formData) => axios.post('/content/upload', formData)
 export {
   getList,
   getTips,
   getLinks,
-  getTop
+  getTop,
+  uploadImg
 }
--- src/components/user/common/PicUpload.vue
+++ src/components/user/common/PicUpload.vue
@@ -4,10 +4,12 @@
             <div class="layui-form-item">
               <div class="avatar-add">
                 <p>建议尺寸168*168，支持jpg、png、gif，最大不能超过50KB</p>
-                <button type="button" class="layui-btn upload-img">
+                <label for="pic" class="layui-btn upload-img">
                   <i class="layui-icon">&#xe67c;</i>上传头像
-                </button>
-                <img src="https://tva1.sinaimg.cn/crop.0.0.118.118.180/5db11ff4gw1e77d3nqrv8j203b03cweg.jpg">
+                </label>
+                <input id="pic" type="file" name="file"
+                  accept="image/png,image/gif,image/jpg" @change="upload">
+                <img :src="pic">
                 <span class="loading"></span>
               </div>
             </div>
@@ -15,11 +17,50 @@
 </template>
 
 <script>
+import { uploadImg } from '@/api/content'
+// import { updateUserInfo } from '@/api/user'
 export default {
-  name: 'picupload'
+  name: 'picupload',
+  data () {
+    return {
+      // 判断 userInfo & pic 是否存在
+      pic: (this.$store.state.userInfo && this.$store.state.userInfo.pic)
+        ? this.$store.state.userInfo.pic : '/img/cat.jpg',
+      formData: ''
+    }
+  },
+  methods: {
+    // 获取准备上传的图片数据
+    upload (e) {
+      console.log(e)
+      let file = e.target.files
+      // FormData对象可以将form表单元素的name与value进行组合
+      let formData = new FormData()
+      if (file.length > 0) {
+        formData.append('file', file[0])
+        this.formData = formData
+      }
+      // 上传图片之后
+      uploadImg(this.formData).then((res) => {
+        console.log(res)
+        // updateUserInfo({ pic: this.pic }).then((res) => {
+        //   if (res.code === 200) {
+        //     // 修改全局的store内的用户基础信息
+        //     let user = this.$store.state.userInfo
+        //     user.pic = this.pic
+        //     this.$store.commit('setUserInfo', user)
+        //     this.$alert('图片上传成功')
+        //   }
+        // })
+      })
+      // 更新用户基本资料
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
-
+#pic {
+  display: none
+}
 </style>
